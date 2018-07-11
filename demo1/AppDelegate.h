525500a205c71d38af243f2b6e9fd2e749b8fd2d
928593829@qq.com <928593829@qq.com>
2018-07-11 16:54:58 +0800

    Initial Commit

---
+++ demo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo1
+//
+//  Created by xhkj on 2018/7/11.
+//  Copyright © 2018年 mmkj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo1
+//
+//  Created by xhkj on 2018/7/11.
+//  Copyright © 2018年 mmkj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
